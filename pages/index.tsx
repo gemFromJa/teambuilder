67f1293e7eaf5eae0029dd44978d207f4bf00f9f
Stephan Woodhouse <stephankw16@gmail.com>
2024-01-12 18:51:07 -0500

    redirect to game page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,11 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
 export default function Home() {
+    const router = useRouter();
+    useEffect(() => {
+        router.replace("/game/");
+    }, []);
     return (
         <>
             <center className="text-4xl mt-[30%]">Coming Soon!!</center>
