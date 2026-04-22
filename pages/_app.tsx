6d7d86c1c1f5f7340737067d1ace65dc99fc6aac
gemFromJa <stephankw16@gmail.com>
2026-04-22 01:33:22 -0500

    Update fonts: Utilize inter font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,20 @@ import Page from "@/components/misc/page";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import AppContext from "@/store";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-inter",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-    return (
-        <AppContext>
-            <Page>
-                <Component {...pageProps} />
-            </Page>
-        </AppContext>
-    );
+  return (
+    <AppContext>
+      <Page className={inter.className}>
+        <Component {...pageProps} />
+      </Page>
+    </AppContext>
+  );
 }
